03a1e4b80d9129c74589bdf553dd3e8dd33c2113
AnettVera <20223tn034@utez.edu.mx>
2025-07-15 15:32:31 -0600

    [FIRST COMMIT] creación de proyecto

---
+++ src/test/java/mx/edu/utez/sima/SimaApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.sima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
